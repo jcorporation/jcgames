a418000af33574ebba49a88aa521a2240695e6e4
jcorporation <mail@jcgames.de>
2023-01-02 21:36:33 +0100

    Fix: scripts

--- .scripts/aktuelles.sh
+++ .scripts/aktuelles.sh
@@ -3,17 +3,17 @@
 for F in ../../spacepirates/github/collections/_posts/*.md
 do
     G=$(basename "$F" .md)
-    sed 's|{{ site.baseurl }}|https://spacepirates.jcgames.de|g' $F | sed -E 's|permalink: (.*)|permalink: \1-spacepirates|' > _posts/$G-spacepirates.md
+    sed 's|{{\s*site.baseurl\s*}}|https://spacepirates.jcgames.de|g' $F | sed -E 's|permalink: (.*)|permalink: \1-spacepirates|' > _posts/$G-spacepirates.md
 done
 
 for F in ../../lite/github/_posts/*.md
 do
     G=$(basename "$F" .md)
-    sed 's|{{ site.baseurl }}|https://lite.jcgames.de|g' $F | sed -E 's|permalink: (.*)|permalink: \1-lite|' > _posts/$G-lite.md
+    sed 's|{{\s*site.baseurl\s*}}|https://lite.jcgames.de|g' $F | sed -E 's|permalink: (.*)|permalink: \1-lite|' > _posts/$G-lite.md
 done
 
 for F in ../../weltenbuchv2/github/collections/_posts/*.md
 do
     G=$(basename "$F" .md)
-    sed 's|{{ site.baseurl }}|https://dasweltenbuch.jcgames.de|g' $F | sed -E 's|permalink: (.*)|permalink: \1-dasweltenbuch|' > _posts/$G-dasweltenbuch.md
+    sed 's|{{\s*site.baseurl\s*}}|https://dasweltenbuch.jcgames.de|g' $F | sed -E 's|permalink: (.*)|permalink: \1-dasweltenbuch|' > _posts/$G-dasweltenbuch.md
 done
--- _posts/2023-01-02-Die-Zeit-vergeht-spacepirates.md
+++ _posts/2023-01-02-Die-Zeit-vergeht-spacepirates.md
@@ -9,7 +9,7 @@ Wie schnell die Zeit doch vergeht...
 
 Da macht man mal kurz etwas anderes und es sind auf einmal 6 Jahre vergangen seitdem das letzte SpacePirates Grundregelwerk erschienen und SpacePirates mitten in der Pubertät ist, 2008 habe ich damit begonnen. Somit wird SpacePirates 15 Jahre alt. Wenn man das nun verdoppelt und 12 addiert ergibt das 42, das muss etwas zu bedeuten haben!
 
-Von daher habe ich jetzt begonnen wieder etwas an SpacePirates zu arbeiten. Die [Sternenkarte]({{site.baseurl}}/Weltraum/Sternenkarte/) wurde bereits aktualisiert und die [Mackentabelle]({{site.baseurl}}/Spielregeln/Die_Piratencrew/Ausrüstung/Raumschiffe/Raumschiffmacken) für Raumschiffe wurde erweitert.
+Von daher habe ich jetzt begonnen wieder etwas an SpacePirates zu arbeiten. Die [Sternenkarte](https://spacepirates.jcgames.de/Weltraum/Sternenkarte/) wurde bereits aktualisiert und die [Mackentabelle](https://spacepirates.jcgames.de/Spielregeln/Die_Piratencrew/Ausrüstung/Raumschiffe/Raumschiffmacken) für Raumschiffe wurde erweitert.
 
 Geplant is momentan:
 
