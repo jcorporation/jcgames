182b038448fae0438026338f1e5de54cd8038426
jcorporation <mail@jcgames.de>
2024-07-10 00:24:26 +0200

    Fix: eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+    pluginJs.configs.recommended,
+    {
+        files: [
+            "_site/assets/js/default.js"
+        ],
+        linterOptions: {
+            reportUnusedDisableDirectives: "off"
+        },
+        languageOptions: {
+            sourceType: "script",
+            globals: {
+                ...globals.browser,
+                "Atomics": "readonly",
+                "SharedArrayBuffer": "readonly",
+                "BSN": true,
+                "tabellen": true,
+                "randgen": true
+            }
+        },
+        rules: {
+            "block-scoped-var": "error",
+            "camelcase": "error",
+            "default-case-last": "error",
+            "eqeqeq": ["error", "always", {"null": "ignore"}],
+            "no-alert": "error",
+            "no-caller": "error",
+            "no-console": "off",
+            "no-eq-null": "error",
+            "no-eval": "error",
+            "no-implied-eval": "error",
+            "no-invalid-this": "error",
+            "no-redeclare": ["error", {"builtinGlobals": false}],
+            "no-restricted-globals": "error",
+            "no-restricted-properties": "error",
+            "no-return-assign": "error",
+            "no-self-compare": "error",
+            "no-shadow": "error",
+            "no-shadow-restricted-names": "error",
+            "no-useless-concat": "error",
+            "prefer-const": "error"
+        }
+    }
+];
